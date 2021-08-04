c3f027294965211764ca0a1440e1e293da5ff27f
krishna11 <kolluri18@iitg.ac.in>
2021-08-05 03:00:18 +0530

    testing deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         HI there
+         Hello ALL!!
         </p>
         <a
           className="App-link"
